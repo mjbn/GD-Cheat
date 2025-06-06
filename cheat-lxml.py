43abe5e0d18c3a8823f1c1fc5d503ac794d6b457
M.J. Bagheri Nejad <56052755+mjbn@users.noreply.github.com>
2025-06-06 21:52:01 +0330

    adding git clone section to the code

--- cheat-lxml.py
+++ cheat-lxml.py
@@ -1,3 +1,11 @@
 #!/bin/python3
+from sys import argv
 from lxml import etree as et
 from git import Repo
+
+
+print(argv)
+if argv[1]=="clone":
+    git_url="https://github.com/godotengine/godot.git"
+    repo_dir="./gd"
+    Repo.clone_from(git_url, repo_dir)
